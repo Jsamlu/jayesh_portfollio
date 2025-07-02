4241f348a5ed2af91d740f3415ed0aa6aec4f874
Jsamlu <jayeshsamlu69@gmail.com>
2025-07-03 03:02:27 +0530

    ok

--- src/Home/LandingPage.jsx
+++ src/Home/LandingPage.jsx
@@ -3,16 +3,16 @@
 function LandingPage() {
   return (
     <div className="px-10 sm:py-10 text-left ">
-      <p className="text-[7.5pt] sm:text-[14pt] text-gray-500 leading-tight font-mono ort">
+      <p className="text-[ ] sm:text-[14pt] text-gray-500 leading-tight font-mono ort">
         <i>Hey, My name is</i>{" "}
       </p>
-      <p className="text-[12.5pt] sm:text-[50pt] text-gray-300 leading-tight font-sans font-bold">
+      <p className="text-3xl sm:text-[50pt] text-gray-300 leading-tight font-sans font-bold">
         JAYESH SAMLU
       </p>
       <p className="text-[12.5pt] sm:text-[50pt] text-gray-500 leading-tight font-mono font-bold">
         I BUILD WEB THINGS
       </p>
-      <p className="text-[7.5pt] sm:text-[14pt] text-center text-gray-500 font-mono text-wrap mt-2">
+      <p className="text-[7.5pt] sm:text-[14pt] text-left text-gray-500 font-mono text-wrap mt-2">
         {" "}
         I'm a software developer who enjoys building cool and useful things with
         code. I love learning new stuff, solving problems, and turning ideas
