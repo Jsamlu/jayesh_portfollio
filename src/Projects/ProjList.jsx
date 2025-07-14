1cb3df5c8819d53e03822f49b3c97d2c0fb0a36f
Jsamlu <jayeshsamlu69@gmail.com>
2025-07-15 02:04:21 +0530

    15-07-25

--- src/Projects/ProjList.jsx
+++ src/Projects/ProjList.jsx
@@ -3,9 +3,9 @@ const ProjList = [
     title: "D-STRESS",
     git: "https://github.com/Jsamlu/stressF",
     live: "https://stress-f.vercel.app/",
-    img: "",
+    img: "/proj_img/D-stress.png",
     tech: ["React js", "Spring Boot", "Rest API", "Javascript", "Java"],
-    desc: "",
+    desc: "D-stress is a student-focused platform designed to help users manage stress, build positive habits, and improve overall well-being.",
   },
   {
     title: "D-STRESS",
--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -1,26 +1,36 @@
 import ProjList from "./ProjList";
 
-
 function Projects() {
   return (
-      <div className="min-w-screen min-h-screen  sm:min-h-[70vh] px-10 sm:px-20 bg-[#030303] py-2">
-        <div>
-          {/* heading  */}
-          <h1 className=" text-gray-400 font-sans font-extralight text-2xl text-center underline decoration-1 underline-offset-[10px]">
-            My Recent Works
-          </h1>
-        </div>
-        <div className=" py-12 sm:py-10 sm:px-20 mx-auto grid sm:grid-cols-3 gap-x-5 gap-y-10 ">
-          {/* project display */}
-            {ProjList.map((item, index)=>{
-              return(
-                <div key={index} className="min-h-[200px] sm:min-h-[350px] bg-gray-600" onClick={""}>
-                    
-                </div>
-              );
-            })}
-        </div>
+    <div className="min-w-screen min-h-screen sm:min-h-[70vh] px-5 sm:px-20 bg-[#030303] py-2">
+      <div>
+        {/* heading  */}
+        <h1 className=" text-gray-400 font-sans font-extralight text-2xl text-center underline decoration-1 underline-offset-[10px]">
+          My Recent Works
+        </h1>
       </div>
+      <div className=" py-12 sm:py-10 sm:px-20 mx-auto grid sm:grid-cols-3 gap-x-5 gap-y-10 ">
+        {/* project display */}
+        {ProjList.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className="flex flex-col gap-y-5 rounded-2xl px-5 py-5 min-h-[250px] bg-gradient-to-b from-gray-800 to-gray-600"
+              onClick={""}
+            >
+              <p className="text-left text-[20pt] font-mono text-white">
+                {item.title}
+              </p>
+              <p className="text-[11pt] text-left font-sans text-gray-400"> {item.desc}</p>
+              <div className="flex gap-5">
+                <button className="px-3 py-1 bg-gradient-to-br from-gray-300 to-gray-500 rounded-3xl">Github</button>
+                <button className="px-3 py-1 bg-gradient-to-br from-gray-300 to-gray-500 rounded-3xl">Live</button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
 
